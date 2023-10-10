e609fa5f21e738117243ec9af260dc2ce2680c91
Ijuaa <pierre.colpart@gmail.com>
2023-10-10 11:04:32 +0200

    Bulbasaur !

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,17 @@
+/*function MyTitle() {
+    return <h1>Hello React</h1>;
+  }
+  
+  export default MyTitle;
+  */
+
+function PokemonCard (){
+  return (
+    <figure>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+      <figcaption>Bulbasaur</figcaption>
+    </figure>
+  )
+}
+
+export default PokemonCard
