1f5f558faad7eb2f4b828c941554834d41fd717c
Ijuaa <pierre.colpart@gmail.com>
2023-10-10 10:20:24 +0200

    done pre ex

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+*/
 import './App.css'
+import MyTitle from './components/MyTitle';
 
+function App() {
+  return (
+    <div>
+      <MyTitle />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+/*
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -33,3 +49,4 @@ function App() {
 }
 
 export default App
+*/
